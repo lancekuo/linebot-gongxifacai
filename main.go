8ef5d05509e10691d41c70daa10c706c82364b4a
Lance Kuo <lancekuo@gmail.com>
2022-10-14 11:58:25 -0400

    Test

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	logger.Info("WeekNumner", zap.Any("::", getWeekNumber()))
+	logger.Info("WeekNumner")
 
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
